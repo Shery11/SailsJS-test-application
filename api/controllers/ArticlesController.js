e20f1af5c0cb178372eb003fae6d1488587fc8a4
shery11 <shery_kirmani@hotmail.com>
2017-10-01 15:52:14 +0500

    create article is added using sails backend and frontend routing

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,6 +6,33 @@
  */
 
 module.exports = {
+
+	list: function(req,res){
+		// reaching for model
+		Articles.find({}).exec(function(err,articles){
+			if(err){
+				res.send(500,{error:"Database error"})
+			}
+			res.view('list',{articles:articles})
+		})
+	},
+
+	add :  function(req,res){
+		res.view('add');
+	},
+
+	create : function(req,res){
+		var title = req.body.title;
+		var body = req.body.body;
+
+
+		Articles.create({title:title,body:body}).exec(function(err){
+			if (err) {
+				res.send(500,{error:"Database error"})
+			}
+			res.redirect('/articles/list');
+		})
+	}
 	
 };
 
